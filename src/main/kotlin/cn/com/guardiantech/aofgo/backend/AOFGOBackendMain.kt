a486459e855d4423373a06832a88eed0832f75ba
Codetector <codetector@codetector.cn>
2017-11-29 23:12:14 -0500

    Work on gradle lib gen

--- src/main/kotlin/cn/com/guardiantech/aofgo/backend/AOFGOBackendMain.kt
+++ src/main/kotlin/cn/com/guardiantech/aofgo/backend/AOFGOBackendMain.kt
@@ -1,10 +1,12 @@
 package cn.com.guardiantech.aofgo.backend
 
+import org.slf4j.LoggerFactory
+
 /**
  * Created by Codetector on 29/11/2017.
  * Project aofgo-backend
  */
 
 fun main(args: Array<String>) {
-
+    val logger = LoggerFactory.getLogger("Main")
 }
