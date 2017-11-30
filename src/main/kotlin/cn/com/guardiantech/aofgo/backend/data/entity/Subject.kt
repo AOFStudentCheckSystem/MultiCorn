e7c5a3c070b4cf899fcfaacb658acf0865a5996a
Codetector <codetector@codetector.cn>
2017-11-29 22:40:05 -0500

    Configure allOpen and noArg

---
+++ src/main/kotlin/cn/com/guardiantech/aofgo/backend/data/entity/Subject.kt
@@ -0,0 +1,13 @@
+package cn.com.guardiantech.aofgo.backend.data.entity
+
+import cn.com.guardiantech.aofgo.backend.annotation.NoArg
+import javax.persistence.Entity
+
+/**
+ * Created by Codetector on 29/11/2017.
+ * Project aofgo-backend
+ */
+@NoArg
+@Entity
+class Subject {
+}
