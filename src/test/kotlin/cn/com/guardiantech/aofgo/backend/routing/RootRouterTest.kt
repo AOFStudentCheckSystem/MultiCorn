f43f69ce8036bf92c48861d611e6d2cc2ffe7d15
Codetector <codetector@codetector.cn>
2017-12-03 15:42:45 -0500

    Test Now Works

--- src/test/kotlin/cn/com/guardiantech/aofgo/backend/routing/RootRouterTest.kt
+++ src/test/kotlin/cn/com/guardiantech/aofgo/backend/routing/RootRouterTest.kt
@@ -1,10 +1,14 @@
 package cn.com.guardiantech.aofgo.backend.routing
 
+import com.nhaarman.mockito_kotlin.mock
 import io.vertx.core.Vertx
+import io.vertx.core.impl.VertxImpl
 import org.junit.Test
 
 import org.junit.Assert.*
-import org.mockito.Mockito.*
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+
 /**
  * Created by Codetector on 03/12/2017.
  * Project aofgo-backend
@@ -13,9 +17,11 @@ open class RootRouterTest {
 
     @Test
     fun testInitialize() {
-        val vertxMock = mock(Vertx::class.java)
+        val vertxMock = Mockito.mock(VertxImpl::class.java)
         val rootRouter = RootRouter(basePackage = "cn.com.guardiantech.aofgo.backend.routing.testPackage", vertx = vertxMock)
         rootRouter.initialize()
         assertEquals(1, rootRouter.controllerMapping.size)
+        assertEquals(1, rootRouter.routeMapping.size)
+        assertEquals("/test/subPath", rootRouter.routeMapping.keys.first().route)
     }
 }
