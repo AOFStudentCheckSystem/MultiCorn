2573eb57b250d7b593c8b0bd120a796427f2d091
DE_DZ_TBH <peiqial@outlook.com>
2018-01-06 20:22:02 -0500

    EventGroupController

--- src/main/kotlin/cn/com/guardiantech/aofgo/backend/controller/checkin/EventGroupController.kt
+++ src/main/kotlin/cn/com/guardiantech/aofgo/backend/controller/checkin/EventGroupController.kt
@@ -1,12 +1,16 @@
 package cn.com.guardiantech.aofgo.backend.controller.checkin
 
+import cn.com.guardiantech.aofgo.backend.data.entity.checkin.EventGroup
 import cn.com.guardiantech.aofgo.backend.request.checkin.CREventToGroupRequest
-import cn.com.guardiantech.aofgo.backend.request.checkin.CreateGroupRequest
+import cn.com.guardiantech.aofgo.backend.request.checkin.GroupRequest
 import cn.com.guardiantech.aofgo.backend.service.checkin.EventGroupService
 import javassist.NotFoundException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
@@ -24,7 +28,7 @@ class EventGroupController @Autowired constructor(
     private val logger: Logger = LoggerFactory.getLogger(EventGroupController::class.java)
 
     @RequestMapping(path = ["/create"], method = [RequestMethod.POST])
-    fun createGroup(@RequestBody @Valid request: CreateGroupRequest) = try {
+    fun createGroup(@RequestBody @Valid request: GroupRequest) = try {
         eventGroupService.createGroup(request)
     } catch (e: NoSuchElementException) {
         throw NotFoundException("Group not found")
@@ -38,4 +42,52 @@ class EventGroupController @Autowired constructor(
         throw NotFoundException("Group or event not found")
     }
 
+    @RequestMapping(path = ["/edit/{groupId}/remove"], method = [RequestMethod.POST])
+    fun removeEventFromGroup(@RequestBody @Valid request: CREventToGroupRequest,
+                             @PathVariable("groupId") groupId: Long) = try {
+        eventGroupService.removeEventFromGroup(request, groupId)
+    } catch (e: NoSuchElementException) {
+        throw NotFoundException("Group or event not found")
+    }
+
+    @RequestMapping(path = ["/edit/{groupId}/set"], method = [RequestMethod.POST])
+    fun setEventToGroup(@RequestBody eventIds: Array<Long>,
+                        @PathVariable("groupId") groupId: Long) = try {
+        eventGroupService.setEventToGroup(eventIds, groupId)
+    } catch (e: NoSuchElementException) {
+        throw NotFoundException("Group or event not found")
+    }
+
+    @RequestMapping(path = ["/edit/{groupId}"], method = [RequestMethod.POST])
+    fun updateEventTitle(@RequestBody @Valid request: GroupRequest,
+                         @PathVariable("groupId") groupId: Long) = try {
+        eventGroupService.updateEventTitle(request, groupId)
+    } catch (e: NoSuchElementException) {
+        throw NotFoundException("Group not found")
+    }
+
+
+    @RequestMapping(path = ["/remove/{id}"], method = [RequestMethod.DELETE])
+    fun removeEventGroupById(@PathVariable id: Long): Long =
+            eventGroupService.removeEventGroupById(id)
+
+    @RequestMapping(path = ["/list"])
+    fun listEventGroups(pageable: Pageable): Page<EventGroup> =
+            eventGroupService.listEventGroups(pageable)
+
+    @GetMapping(path = ["/list/{id}"])
+    fun getEventGroupById(@PathVariable id: Long) = try {
+        eventGroupService.getEventGroupById(id)
+    } catch (e: NoSuchElementException) {
+        throw NotFoundException("Group not found")
+    }
+
+    @GetMapping(path = ["/listall"])
+    fun listAllEventGroups() =
+            listEventGroups(
+                    PageRequest(0, Int.MAX_VALUE))
+
+    @GetMapping(path = ["/list-available"])
+    fun listAvailable() =
+            eventGroupService.listAvailable()
 }
--- src/main/kotlin/cn/com/guardiantech/aofgo/backend/service/checkin/EventGroupService.kt
+++ src/main/kotlin/cn/com/guardiantech/aofgo/backend/service/checkin/EventGroupService.kt
@@ -5,9 +5,13 @@ import cn.com.guardiantech.aofgo.backend.data.entity.checkin.EventGroup
 import cn.com.guardiantech.aofgo.backend.repository.checkin.EventGroupRepository
 import cn.com.guardiantech.aofgo.backend.repository.checkin.EventRepository
 import cn.com.guardiantech.aofgo.backend.request.checkin.CREventToGroupRequest
-import cn.com.guardiantech.aofgo.backend.request.checkin.CreateGroupRequest
+import cn.com.guardiantech.aofgo.backend.request.checkin.GroupRequest
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.PathVariable
+import java.util.*
 
 /**
  * Created by dedztbh on 1/6/18.
@@ -23,7 +27,7 @@ class EventGroupService @Autowired constructor(
     /**
      * @throws NoSuchElementException Group not found
      */
-    fun createGroup(request: CreateGroupRequest) {
+    fun createGroup(request: GroupRequest) {
         val events: MutableSet<ActivityEvent> = hashSetOf()
         request.groupItems.forEach {
             events.add(eventRepo.findByEventId(it).get())
@@ -41,4 +45,51 @@ class EventGroupService @Autowired constructor(
         eventGroupRepo.save(targetGroup)
     }
 
+    /**
+     * @throws NoSuchElementException Group or event not found
+     */
+    fun removeEventFromGroup(request: CREventToGroupRequest, groupId: Long) {
+        val targetGroup = eventGroupRepo.findById(groupId).get()
+        val targetEvent = eventRepo.findByEventId(request.eventId).get()
+        val result = targetGroup.events.remove(targetEvent)
+        eventGroupRepo.save(targetGroup)
+    }
+
+    /**
+     * @throws NoSuchElementException Group or event not found
+     */
+    fun setEventToGroup(items: Array<Long>, groupId: Long) {
+        val targetGroup = eventGroupRepo.findById(groupId).get()
+        val events: MutableList<ActivityEvent> = arrayListOf()
+        items.forEach {
+            events.add(eventRepo.findByEventId(it.toString()).get())
+        }
+        targetGroup.events.clear()
+        targetGroup.events.addAll(events)
+        eventGroupRepo.save(targetGroup)
+    }
+
+    /**
+     * @throws NoSuchElementException group not found
+     */
+    fun updateEventTitle(request: GroupRequest, groupId: Long) {
+        val targetGroup = eventGroupRepo.findById(groupId).get()
+        targetGroup.name = request.name
+        eventGroupRepo.save(targetGroup)
+    }
+
+    fun removeEventGroupById(id: Long): Long =
+            eventGroupRepo.removeById(id)
+
+    fun listEventGroups(pageable: Pageable): Page<EventGroup> =
+            eventGroupRepo.findAllByOrderByIdDesc(pageable)
+
+    /**
+     * @throws NoSuchElementException group not found
+     */
+    fun getEventGroupById(@PathVariable id: Long): EventGroup =
+            eventGroupRepo.findById(id).get()
+
+    fun listAvailable(): List<EventGroup> =
+            eventGroupRepo.findGroupWithoutBinding()
 }
