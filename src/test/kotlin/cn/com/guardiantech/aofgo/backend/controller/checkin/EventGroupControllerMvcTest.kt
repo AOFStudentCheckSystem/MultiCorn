715928f0e3c4ef5692cea5d7a0540659d59a54be
calvinx <calvinxu806@gmail.com>
2018-01-08 19:23:47 -0500

    Started unit test of check in controller

---
+++ src/test/kotlin/cn/com/guardiantech/aofgo/backend/controller/checkin/EventGroupControllerMvcTest.kt
@@ -0,0 +1,26 @@
+//package cn.com.guardiantech.aofgo.backend.controller.checkin
+//
+//import cn.com.guardiantech.aofgo.backend.BackendApplication
+//import cn.com.guardiantech.aofgo.backend.BackendApplicationTestConfiguration
+//import org.junit.runner.RunWith
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+//import org.springframework.boot.test.context.SpringBootTest
+//import org.springframework.context.annotation.Import
+//import org.springframework.data.web.config.EnableSpringDataWebSupport
+//import org.springframework.test.context.junit4.SpringRunner
+//
+///**
+// * Created by calvinx on 2018/01/08.
+// * Project AOFGoBackend
+// */
+//
+//@RunWith(SpringRunner::class)
+//@SpringBootTest(classes = [BackendApplication::class])
+//@DataJpaTest
+//@Import(BackendApplicationTestConfiguration::class)
+//@AutoConfigureMockMvc
+//@EnableSpringDataWebSupport
+//class EventGroupControllerMvcTest {
+//
+//}
