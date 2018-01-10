2858043313a20bfdcce9859ea5cb1a2d1646818c
DE_DZ_TBH <peiqial@outlook.com>
2018-01-10 15:38:24 -0500

    created jsonignore for Credential #AOFGO-61 Done

--- src/main/kotlin/cn/com/guardiantech/aofgo/backend/data/entity/authentication/Credential.kt
+++ src/main/kotlin/cn/com/guardiantech/aofgo/backend/data/entity/authentication/Credential.kt
@@ -1,6 +1,7 @@
 package cn.com.guardiantech.aofgo.backend.data.entity.authentication
 
 import com.fasterxml.jackson.annotation.JsonBackReference
+import com.fasterxml.jackson.annotation.JsonIgnore
 import org.hibernate.annotations.UpdateTimestamp
 import java.util.*
 import javax.persistence.*
@@ -21,8 +22,8 @@ class Credential (
 
         @Lob
         @Column(name = "credential_secret", nullable = false)
+        @JsonIgnore
         var secret: String,
-
 
         @ManyToOne
         @JsonBackReference
@@ -32,8 +33,10 @@ class Credential (
         @ElementCollection
         @CollectionTable(name = "credential_secret_history", joinColumns = [(JoinColumn(name = "id"))])
         @Column(name = "secret")
+        @JsonIgnore
         var historySecrets: MutableSet<String> = hashSetOf(),
 
         @UpdateTimestamp
+        @JsonIgnore
         var lastChange: Date = Date()
 )
