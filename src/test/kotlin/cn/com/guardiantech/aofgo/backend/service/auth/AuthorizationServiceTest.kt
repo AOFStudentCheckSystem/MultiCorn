1d0ac6bd95d40a0a95551baf5c978ab4a189e23a
DE_DZ_TBH <peiqial@outlook.com>
2018-02-10 15:09:39 -0500

    Error: Failed to save Subject during edit * 2

--- src/test/kotlin/cn/com/guardiantech/aofgo/backend/service/auth/AuthorizationServiceTest.kt
+++ src/test/kotlin/cn/com/guardiantech/aofgo/backend/service/auth/AuthorizationServiceTest.kt
@@ -1,4 +1,4 @@
-package cn.com.guardiantech.aofgo.backend.controller
+package cn.com.guardiantech.aofgo.backend.service.auth
 
 import cn.com.guardiantech.aofgo.backend.BackendApplication
 import cn.com.guardiantech.aofgo.backend.BackendApplicationTestConfiguration
@@ -6,39 +6,29 @@ import cn.com.guardiantech.aofgo.backend.data.entity.authentication.Role
 import cn.com.guardiantech.aofgo.backend.data.entity.authentication.Subject
 import cn.com.guardiantech.aofgo.backend.repository.auth.RoleRepository
 import cn.com.guardiantech.aofgo.backend.repository.auth.SubjectRepository
-import cn.com.guardiantech.aofgo.backend.test.authutil.AuthenticationUtil
-import org.junit.Assert.*
-import org.junit.Assert
-import org.junit.Before
+import cn.com.guardiantech.aofgo.backend.request.authentication.admin.SubjectEditRequest
 import org.junit.Test
+
+import org.junit.Assert.*
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
-import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 /**
- * Created by dedztbh on 18-2-4.
+ * Created by dedztbh on 18-2-10.
  * Project AOFGoBackend
  */
 @RunWith(SpringRunner::class)
 @SpringBootTest(classes = [BackendApplication::class])
 @DataJpaTest
 @Import(BackendApplicationTestConfiguration::class)
-@AutoConfigureMockMvc
-class AuthenticationAdminControllerTest {
+class AuthorizationServiceTest {
 
     @Autowired
-    private lateinit var mockMvc: MockMvc
-
-    @Autowired
-    private lateinit var authUtil: AuthenticationUtil
+    lateinit var authorizationService: AuthorizationService
 
     @Autowired
     private lateinit var subjectRepository: SubjectRepository
@@ -46,32 +36,17 @@ class AuthenticationAdminControllerTest {
     @Autowired
     private lateinit var roleRepository: RoleRepository
 
-    @Before
-    fun initialize() {
-        Assert.assertNotNull(mockMvc)
-        authUtil.prepare()
-    }
-
     @Test
     fun editSubjectSetRole() {
         val newSubjectId = subjectRepository.save(Subject()).id
         val newRoleId = roleRepository.save(Role(roleName = "nnp")).id
-
-        mockMvc
-                .perform(MockMvcRequestBuilders.post("/auth/admin/subject")
-                        .with({
-                            it.addHeader("Authorization", authUtil.getSession().sessionKey)
-                            it
-                        })
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .content("""
-                    {
-                        "id":$newSubjectId,
-                        "subjectAttachedInfo":"nil",
-                        "roles":["nnp"]
-                    }
-                """.trimIndent()))
-                .andExpect(MockMvcResultMatchers.status().isOk)
+        authorizationService.editSubjectSetRole(
+                SubjectEditRequest(
+                        id = newSubjectId,
+                        subjectAttachedInfo = "nil",
+                        roles = setOf("nnp")
+                )
+        )
         val s = subjectRepository.findById(newSubjectId).get()
         assertEquals("nil", s.subjectAttachedInfo)
         assertEquals(1L, s.roles)
