ab51d83e56f60c30004dd06da72e871af249a0ff
ArtemisZX <calvinxu806@gmail.com>
2018-02-14 19:30:47 -0500

    Fix bad things

---
+++ src/main/kotlin/cn/com/guardiantech/aofgo/backend/exception/ImportBadConstraintException.kt
@@ -0,0 +1,7 @@
+package cn.com.guardiantech.aofgo.backend.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+class ImportBadConstraintException(message: String?): ControllerException(message)
