547de2c164f210e1c07a4797dda4773d971821b3
DE_DZ_TBH <peiqial@outlook.com>
2018-03-27 16:15:27 -0400

    data structure change and set slip status api

--- src/main/kotlin/cn/com/guardiantech/aofgo/backend/data/entity/GuardianType.kt
+++ src/main/kotlin/cn/com/guardiantech/aofgo/backend/data/entity/GuardianType.kt
@@ -1,5 +1,5 @@
 package cn.com.guardiantech.aofgo.backend.data.entity
 
 enum class GuardianType {
-    PARENT, ACADEMIC_DEAN, COACH, FACULTY_SUPERVISOR, ADVISOR, ASSOCIATE_HEADMASTER
+    PARENT, ACADEMIC_DEAN, COACH, FACULTY_SUPERVISOR, ADVISOR
 }
